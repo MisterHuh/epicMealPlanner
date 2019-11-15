37f09b30482a60d1bce7a31fe7eddfead1c51490
MisterHuh <jaehuh86@yahoo.com>
2019-11-15 13:42:52 -0800

    fixed header styling

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,12 +3,12 @@ import Menu from './menu';
 
 function Header(props) {
   return (
-    <div className="container">
-      <div className="row justify-content-end mx-3 mt-5 mb-1">
-        <div className="headerTextMenu textFont textSize flex-fill align-self-center text-capitalize">{ props.text }</div>
+    // <div className="container">
+      <div className="row justify-content-end px-4 pt-2 pb-1 empHeader">
+        <div className="d-inline flex-fill align-self-center text-capitalize">{ props.text }</div>
         <Menu setView={ props.setView }/>
       </div>
-    </div>
+    // </div>
   );
 }
 
